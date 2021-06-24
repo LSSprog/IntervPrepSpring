49a81aa49397fd9f03b4f03336c3356f8b26fa1c
LSSprog <fedexss@bk.ru>
2021-06-24 13:05:55 +0300

    ДЗ к 7 уроку (first commit)

---
+++ src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,4 @@
+CREATE TABLE students_tbl (
+    student_id      bigserial PRIMARY KEY,
+    name_fld
+)
---
+++ src/test/java/lss/spring/interview/Hw7ApplicationTests.java
@@ -0,0 +1,13 @@
+package lss.spring.interview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
